8c3348d9e60fd09ea0cc74c431b7dcf4462f1ffb
cedriclecalvez <cedric.lecalvez@atos.net>
2022-02-08 21:46:22 +0100

    refactor before routing, change app to appareil view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,36 +7,6 @@ import { AppareilService } from './services/appareil.service';
   styleUrls: ['./app.component.scss'],
 })
 
-// implements OnInit permet d'utiliser le ngOnInit juste apres la constrution du composant
-export class AppComponent implements OnInit {
-  isAuth = false;
-  // fonction promise pour tester le pipe async coté html, demander d'attendre lastdate avant de l'afficher 
-  lastUpdate = new Promise<Date>((resolve, reject) => {
-    const date = new Date();
-    setTimeout(() => {
-      resolve(date);
-    }),
-      2000;
-  });
-
-  // initialize data array
-  appareils: any = [];
-  
-  // declaration variable appareilService venant de AppareilService
-  constructor(private appareilService: AppareilService) {
-    setTimeout(() => (this.isAuth = true)), 4000;
-  }
-
-  ngOnInit() {
-    this.appareils = this.appareilService.appareils;
-  }
-  onAllumer() {
-    console.log('on allume tout');
-    this.appareilService.switchOnAll()
-  }
-  onEteindre() {
-    console.log('on éteint tout');
-    this.appareilService.switchOffAll()
-
-  }
+export class AppComponent {
+  constructor(){}
 }
---
+++ src/app/appareil-view/appareil-view.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { AppareilService } from '../services/appareil.service';
+
+@Component({
+  selector: 'app-appareil-view',
+  templateUrl: './appareil-view.component.html',
+  styleUrls: ['./appareil-view.component.scss']
+})
+
+// implements OnInit permet d'utiliser le ngOnInit juste apres la constrution du composant
+
+export class AppareilViewComponent implements OnInit {
+
+  isAuth = false;
+  // fonction promise pour tester le pipe async coté html, demander d'attendre lastdate avant de l'afficher 
+  lastUpdate = new Promise<Date>((resolve, reject) => {
+    const date = new Date();
+    setTimeout(() => {
+      resolve(date);
+    }),
+      2000;
+  });
+
+  // initialize data array
+  appareils: any = [];
+  
+  // declaration variable appareilService venant de AppareilService
+  constructor(private appareilService: AppareilService) {
+    setTimeout(() => (this.isAuth = true)), 4000;
+  }
+
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
+  }
+  onAllumer() {
+    console.log('on allume tout');
+    this.appareilService.switchOnAll()
+  }
+  onEteindre() {
+    console.log('on éteint tout');
+    this.appareilService.switchOffAll()
+
+  }
+}
