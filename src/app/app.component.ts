685e5799eb07105ea1d14ae4fb056e53ab666c8b
cedriclecalvez <cedric.lecalvez@atos.net>
2022-02-08 14:36:03 +0100

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
  isAuth = false;
 
+appareilOne = "Machine à laver";
+appareilTwo = "Télévision";
+appareilThree = "Ordinateur";
+
  constructor() {
     setTimeout(() => this.isAuth=true),4000
  };
