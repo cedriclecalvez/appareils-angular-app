b88c87baf90c0281cb1b4b8f30454e8ee773e85e
cedriclecalvez <cedric.lecalvez@atos.net>
2022-02-08 15:24:32 +0100

    ngStyle

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -3,18 +3,25 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-appareil',
   templateUrl: './appareil.component.html',
-  styleUrls: ['./appareil.component.scss']
+  styleUrls: ['./appareil.component.scss'],
 })
 export class AppareilComponent implements OnInit {
-
   @Input() appareilName!: string;
   @Input() appareilStatus!: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getStatus(): string {
+    return this.appareilStatus;
   }
 
-  getStatus(): string { return this.appareilStatus}
-
+  getColor(): string|any {
+    if (this.appareilStatus === 'allumé') {
+      return 'green';
+    } else if (this.appareilStatus === 'éteint') {
+      return 'red';
+    }
+  }
 }
