e927404acd8969be05d40ff2e08461415aaca5e8
cedriclecalvez <cedric.lecalvez@atos.net>
2022-02-08 21:57:33 +0100

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { AppareilComponent } from './appareil/appareil.component';
 import { AppareilService } from './services/appareil.service';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes:Routes = [
+  { path: 'appareils', component:AppareilViewComponent},
+  { path: 'auth', component:AuthComponent},
+  { path: '', component:AppareilViewComponent},
+]
 
 @NgModule({
   declarations: [
@@ -20,7 +27,8 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes) //permet de dire a Angular que toutes les routes se trouvent dans appRoutes
   ],
   // mettre les services dans providers
   providers: [AppareilService
