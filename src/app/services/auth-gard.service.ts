b571c9060cf9f07b879bbffb3d8b705e5227a0b9
cedriclecalvez <cedric.lecalvez@atos.net>
2022-02-10 15:15:06 +0100

    add message when didnt login

--- src/app/services/auth-gard.service.ts
+++ src/app/services/auth-gard.service.ts
@@ -1,24 +1,28 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 // import { Observable } from 'rxjs/Observable';
 import { Observable } from 'rxjs/internal/Observable';
 import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGardService implements CanActivate{
-  
-  constructor(private authService: AuthService, private router: Router) { }
+export class AuthGardService implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-    ): Observable<boolean> | Promise<boolean> | boolean {
-      if (this.authService.isAuth) {
-        return true;    
-      }else {
-        return this.router.navigate(['/auth'])
-      }
-      
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isAuth) {
+      return true;
+    } else {
+      console.error('tu dois te logger avant');
+      return this.router.navigate(['/auth']);
     }
+  }
 }
