3bc39a6869f440e6f9c7445bf7becadc7cdf80cb
cedriclecalvez <cedric.lecalvez@atos.net>
2022-02-10 16:33:47 +0100

    create a counter with interval observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
 
 
 @Component({
@@ -7,6 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   constructor(){}
+  
+  secondes!:number;
+  counterSubscription!:Subscription;
+
+  ngOnInit(): void {
+
+    // const counter = interval(1000).subscribe({
+    //   next: this.secondes = value,
+    //   error: console.log("Une erreur a été rencontrée"),
+    //   complete: console.log("Observable complètée!")
+    // });
+    
+      
+    const counter = interval(1000)
+    this.counterSubscription=counter.subscribe(
+      (value:number) => {
+        this.secondes = value;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    // pour detruire la subscription qui etait sur un interval infinie
+    this.counterSubscription.unsubscribe()
+  }
 }
