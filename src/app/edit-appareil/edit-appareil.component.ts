d2fc30d8e95e4acb16abd6b1c8bb7c1d170ccb8f
cedriclecalvez <cedric.lecalvez@atos.net>
2022-02-10 22:20:22 +0100

    create data validation

--- src/app/edit-appareil/edit-appareil.component.ts
+++ src/app/edit-appareil/edit-appareil.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class EditAppareilComponent implements OnInit {
 
+  defaultOnOff: string = 'éteint'
+
   constructor() { }
 
   ngOnInit(): void {
